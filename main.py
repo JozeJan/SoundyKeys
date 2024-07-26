e0dce09c2144504b3a4a4452c99fca808bf6e407
WindowsServer <jozejan23@gmail.com>
2024-07-26 12:31:43 +0100

    Work

--- main.py
+++ main.py
@@ -1,14 +1,16 @@
 import keyboard
 import pygame
 keyboardpressnum = 1
+pygame.mixer.init()
+am_channel = pygame.mixer.Channel(1)
+
 while True:
-    # Wait for the next event.
     event = keyboard.read_event()
     if event.event_type == keyboard.KEY_DOWN:
-        print(event.name) # to check key name
-        pygame.mixer.init()
-        pygame.mixer.music.load(f"Audio/{keyboardpressnum}.wav")
-        pygame.mixer.music.play()
+        am_am_ams = pygame.mixer.Sound(f"Audio/{keyboardpressnum}.wav")
+        print(event.name)
+        am_channel.play(am_am_ams)
         keyboardpressnum += 1
         if keyboardpressnum > 15: #How many times to am am am
             keyboardpressnum = 1
+            
