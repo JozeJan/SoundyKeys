e4b90e0b33ad1531b903163a1b2c8c761ad2d8cd
WindowsServer <jozejan23@gmail.com>
2024-07-26 17:49:35 +0100

    Has an option to swich modes with f2 key

--- main.py
+++ main.py
@@ -3,9 +3,9 @@ import pygame
 keyboardpressnum = 1
 pygame.mixer.init()
 am_channel = pygame.mixer.Channel(1)
-
-while True:
-    event = keyboard.read_event()
+menu = "am"
+event = keyboard.read_event()
+while menu == "am":
     if event.event_type == keyboard.KEY_DOWN:
         am_am_ams = pygame.mixer.Sound(f"Audio/{keyboardpressnum}.wav")
         print(event.name)
@@ -13,4 +13,15 @@ while True:
         keyboardpressnum += 1
         if keyboardpressnum > 15: #How many times to am am am
             keyboardpressnum = 1
-            
+        if keyboard.is_pressed('f2'):  # if key 'q' is pressed
+            print('You Pressed A Key!')
+            menu = "štefan"
+while menu == "štefan":
+
+
+
+
+
+
+
+
